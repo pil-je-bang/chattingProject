e60bba5ba01ade94cbc2a491ae0fc6e33e1399d2
Park <ppsw4488@naver.com>
2023-10-11 15:24:04 +0900

    same_id_detector

--- join/join/join.cpp
+++ join/join/join.cpp
@@ -23,6 +23,11 @@ int main()
     sql::Statement* stmt;
     sql::PreparedStatement* pstmt;
     sql::ResultSet* res = NULL;
+    int choice = 0;
+    string in = "";
+    string pw = "";
+    bool is_there_same = 1;
+
     try {
         driver = sql::mysql::get_mysql_driver_instance();
         con = driver->connect(server, username, password);
@@ -34,7 +39,7 @@ int main()
 
     // 데이터베이스 선택
     con->setSchema("final");
-
+    
     // db 한글 저장을 위한 셋팅 
     stmt = con->createStatement();
     stmt->execute("set names euckr");
@@ -42,33 +47,48 @@ int main()
 
     // 데이터베이스 쿼리 실행
     stmt = con->createStatement();
-
-
-    cout << "id :";
-    string in = "";
-    string pw = "";
-    getline(cin, in);
-    cout << "pw :";
-    getline(cin, pw);
-
-    res = stmt->executeQuery("SELECT id FROM user_info");
-
-    cout << "===========id list=============" << endl;
-    while (res->next() == true) {
-        std::string id = res->getString("id");
-        if (in == id) { cout << "!!"; }
+    /*stmt->execute("DROP TABLE IF EXISTS user_info");// 채팅 시작 할때 생성해야 됨
+    cout << "Finished dropping table (if existed)" << endl;// DROP
+    stmt->execute("CREATE TABLE user_info (id VARCHAR(50) PRIMARY key , name VARCHAR(50));"); // CREATE
+    cout << "Finished creating table" << endl;*/
+    while (is_there_same) {
+        cout << "id :";
+        getline(cin, in);
+        res = stmt->executeQuery("SELECT id FROM user_info");
+        while (res->next() == true) {
+            std::string id = res->getString("id");
+            if (in == id) {
+                is_there_same = true; cout << "이미 존재하는 아이디 입니다." << endl;
+                Sleep(500);
+                break;
+            }
+            else
+                is_there_same = false;
+        }
+        system("cls");
+    }
+    if (is_there_same) {}
+    else {
+        pstmt = con->prepareStatement("INSERT INTO user_info(id, name ) VALUES(?,?)"); // INSERT
+        pstmt->setString(1, in);
+        pstmt->setString(2, "john");
+        pstmt->execute();
+        cout << "가입완료" << endl;
+        delete pstmt;
     }
 
+
+    cout << "===========id list=============" << endl;
+    
+    while (res->next() == true) {
+        std::string id = res->getString("id");
+        if (in == id) { is_there_same = true; }
+    }
     delete stmt;
-
-
-
-
-
 
     // MySQL Connector/C++ 정리
     delete res;
- 
+
     delete con;
 
     return 0;
