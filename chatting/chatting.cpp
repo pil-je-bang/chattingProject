e413f757bce23d2a704f63eee31ccb8233db9ca3
방필제 <ppjj2444@gmail.com>
2023-10-12 14:40:19 +0900

    chatting

--- chatting/chatting.cpp
+++ chatting/chatting.cpp
@@ -1,66 +1,121 @@
+#pragma comment(lib, "ws2_32.lib")
+
+#include <WinSock2.h> //Winsock 헤더파일 include. WSADATA 들어있음.
+#include <WS2tcpip.h>
 #include <iostream>
 #include <string>
 #include <mysql/jdbc.h>
+#include <thread>
+#include <sstream>
+#include <chrono>
+#include <iomanip>
+
+#define MAX_SIZE 1024
 
 using std::cout;
 using std::cin;
 using std::endl;
 using std::string;
+using std::to_string;
 
-const string server = "tcp://127.0.0.1:3306"; // 데이터베이스 주소
-const string username = "root"; // 데이터베이스 사용자
-const string password = "(()()&pj0907"; // 데이터베이스 접속 비밀번호
+SOCKET client_sock;
+string my_nick;
+
+int chat_recv() {
+    char buf[MAX_SIZE] = { };
+    string msg;
+
+    while (1) {
+        ZeroMemory(&buf, MAX_SIZE);
+        if (recv(client_sock, buf, MAX_SIZE, 0) > 0) {
+            msg = buf;
+            std::stringstream ss(msg);  // 문자열을 스트림화
+            string user;
+            ss >> user; // 스트림을 통해, 문자열을 공백 분리해 변수에 할당. 보낸 사람의 이름만 user에 저장됨.
+            if (user != my_nick) {
+                cout << buf << endl; // 내가 보낸 게 아닐 경우에만 출력하도록.
+            }
+        }
+        else {
+            cout << "Server Off" << endl;
+            return -1;
+        }
+    }
+}
+
+string get_time()
+{
+    using namespace std::chrono;
+    system_clock::time_point tp = system_clock::now();
+    std::stringstream str;
+    __time64_t t1 = system_clock::to_time_t(tp);
+    system_clock::time_point t2 = system_clock::from_time_t(t1);
+    if (t2 > tp)
+        t1 = system_clock::to_time_t(tp - seconds(1));
+
+    tm tm{};
+    localtime_s(&tm, &t1);
+
+    str << std::put_time(&tm, "[%Y-%m-%d %H:%M:%S]") << std::setfill('0') << std::setw(3)
+        << (std::chrono::duration_cast<std::chrono::milliseconds>(tp.time_since_epoch()).count() % 1000);
+
+    return str.str();
+}
+
+
+
+
 
 int main()
 {
-    // MySQL Connector/C++ 초기화
-    sql::mysql::MySQL_Driver* driver; // 추후 해제하지 않아도 Connector/C++가 자동으로 해제해 줌
-    sql::Connection* con;
-    sql::Statement* stmt;
-    sql::PreparedStatement* pstmt;
+    WSADATA wsa;
 
-    try {
-        driver = sql::mysql::get_mysql_driver_instance();
-        con = driver->connect(server, username, password);
+    // Winsock를 초기화하는 함수. MAKEWORD(2, 2)는 Winsock의 2.2 버전을 사용하겠다는 의미.
+    // 실행에 성공하면 0을, 실패하면 그 이외의 값을 반환.
+    // 0을 반환했다는 것은 Winsock을 사용할 준비가 되었다는 의미.
+    int code = WSAStartup(MAKEWORD(2, 2), &wsa);
+
+    if (!code) {
+        cout << "사용할 닉네임 입력 >> ";
+        cin >> my_nick;
+
+        client_sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP); // 
+
+        // 연결할 서버 정보 설정 부분
+        SOCKADDR_IN client_addr = {};
+        client_addr.sin_family = AF_INET;
+        client_addr.sin_port = htons(7777);
+        InetPton(AF_INET, TEXT("127.0.0.1"), &client_addr.sin_addr);
+
+        while (1) {
+            if (!connect(client_sock, (SOCKADDR*)&client_addr, sizeof(client_addr))) { // 위에 설정한 정보에 해당하는 server로 연결!
+                cout << "Server Connect" << endl;
+                send(client_sock, my_nick.c_str(), my_nick.length(), 0); // 연결에 성공하면 client 가 입력한 닉네임을 서버로 전송
+                break;
+            }
+            cout << "Connecting..." << endl;
+        }
+
+        std::thread th2(chat_recv);
+        cout << "채팅이 시작되었습니다.";
+        cout << "\n"; 
+        
+        while (1) {
+            string text;
+            /*std::getline(cin, text);*/
+            cin >> text;
+            text += get_time();
+            const char* buffer = text.c_str(); // string형을 char* 타입으로 변환
+            send(client_sock, buffer, strlen(buffer), 0);
+        }
+        th2.join();
+        closesocket(client_sock);
+
+
     }
-    catch (sql::SQLException& e) {
-        cout << "Could not connect to server. Error message: " << e.what() << endl;
-        exit(1);
-    }
 
-    // 데이터베이스 선택
-    con->setSchema("kdt");
-
-    // db 한글 저장을 위한 셋팅 
-    stmt = con->createStatement();
-    stmt->execute("set names euckr");
-    if (stmt) { delete stmt; stmt = nullptr; }
-
-    // 데이터베이스 쿼리 실행
-    stmt = con->createStatement();
-    stmt->execute("DROP TABLE IF EXISTS chatting"); // DROP
-    cout << "Finished dropping table (if existed)" << endl;
-    stmt->execute("CREATE TABLE chatting (id serial PRIMARY KEY, 내용 VARCHAR(255));"); // CREATE
-    cout << "Finished creating table" << endl;
-    delete stmt;
-
-    pstmt = con->prepareStatement("INSERT INTO chatting(내용) VALUES(?)"); // INSERT
-    cout << "채팅이 시작되었습니다.";
-    cout << "\n";
-    string a;
-    while (a != "c") {
-        cin >> a;
-        cout << "\n";
-        pstmt->setString(1, a);
-        pstmt->execute();
-        //cout << "One row inserted." << endl;
-
-    }
-
-
-    // MySQL Connector/C++ 정리
-    delete pstmt;
-    delete con;
+    WSACleanup();
 
     return 0;
 }
+
